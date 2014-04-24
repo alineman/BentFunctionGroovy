f3642590ca4448dbe4fd154a3623101365689930
Ivan Samohvalov <isamohvalov@gmail.com>
2014-04-25 00:33:52 +0400

    first iteartion same bad

---
+++ src/main/java/StringHelper.groovy
@@ -0,0 +1,56 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: ivan
+ * Date: 24.04.14
+ * Time: 22:07
+ * To change this template use File | Settings | File Templates.
+ */
+class StringHelper {
+
+
+    static String binary(int i) {
+        binary(i,log2M(i))
+    }
+
+    static int log2(int x) {
+        int n = 0
+        for (int j = 0; Math.pow(2,j) < x; j++ )
+            n++
+        n
+    }
+
+//    static int log2M(int x) {
+//        (int)Math.ceil(Math.log(x)/Math.log(2))
+//    }
+    static int log2Service(int x){
+        (int)Math.ceil(Math.log(x)/Math.log(2))
+    }
+
+    static int log2M(int x) {
+        (int)Math.ceil(Math.log(x)/Math.log(2))
+    }
+
+    static String binary(int i, int length) {
+        final String zero = "00000000000000000000000000000000"
+
+        String result = Integer.toBinaryString(i)
+        int dif = Math.abs(length - result.length())
+        if (dif > 0)
+            result = zero.substring(0, dif) + result
+
+        result
+    }
+
+    static int roundUpLn(int i) {
+        Integer.toBinaryString(i).length()
+    }
+
+    static void printTableOfFunction(int i) {
+        int power = log2M(i)
+        String stringI = binary(i,power)
+
+        for (int j = 0; j < power; j++) {
+            println(binary(j, log2(power)) + " | " + stringI.charAt(stringI.length() - j - 1))
+        }
+    }
+}
